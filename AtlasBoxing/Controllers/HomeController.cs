cab813d4cdfe4fbf08fd5b76511194079df223a5
Eliza Lenjani <lenjanieliza@outlook.com>
2024-07-18 15:37:26 +0100

    Add .gitignore file to ignore .vs directory

---
+++ AtlasBoxing/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AtlasBoxing.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Meet the Founders";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Find us at";
+
+            return View();
+        }
+
+        public ActionResult Classes()
+        {
+            ViewBag.Message = "Timetables";
+
+
+            return View();
+        }
+        public ActionResult YouthCommunityHub()
+        {
+            ViewBag.Message = "Youth Community Hub";
+
+            return View();
+        }
+        public ActionResult Pricing()
+        {
+            ViewBag.Message = "Pricing";
+
+            return View();
+        }
+
+    }
+}       
